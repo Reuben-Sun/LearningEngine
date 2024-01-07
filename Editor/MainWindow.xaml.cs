19610d3e63e6fe92e95812c64e9752b9cbf96bd0
Reuben-Sun <2824016820@qq.com>
2024-01-07 23:03:20 +0800

    add winform

--- Editor/MainWindow.xaml.cs
+++ Editor/MainWindow.xaml.cs
@@ -2,13 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms.Integration;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -19,17 +22,12 @@ namespace ToolEditor
 {
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        #region Binding
         private string testLabel;
         public string TestLabel
         {
             get { return testLabel; }
             set { testLabel = value; OnPropertyChanged(nameof(TestLabel)); }
-        }
-        public MainWindow()
-        {
-            InitializeComponent();
-            this.DataContext = this;
-            TestLabel = "Hello";
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -37,12 +35,24 @@ namespace ToolEditor
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        #endregion
 
+
+        private IntPtr hwnd;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            TestLabel = "Hello";
+        }
+
+        #region Action
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             EditorEngine engine = new EditorEngine();
-            // EngineRenderModule engineRenderModule = new EngineRenderModule();
             TestLabel = engine.getX().ToString();
         }
+        #endregion
+
     }
 }
