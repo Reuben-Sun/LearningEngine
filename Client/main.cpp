7029e1cc767aa579ffff278c8917a5d22b365809
Reuben-ROG <2824016820@qq.com>
2024-01-09 13:55:12 +0800

    批注

--- Client/main.cpp
+++ Client/main.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include "Engine.h"
 
+_Use_decl_annotations_
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
     Engine::Engine engine = Engine::Engine(800, 600, L"Engine");
--- Engine/Platform/Win32Application.h
+++ Engine/Platform/Win32Application.h
@@ -11,6 +11,7 @@ namespace Engine
 		~Application();
 		void onUpdate();
 		bool needUpdate();
+		HWND getHWND() const { return m_hwnd; }
 	private:
 		DXWindow* m_window{ nullptr };
 		HWND m_hwnd;
